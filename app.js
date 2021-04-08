cd7a782ee565d49a4f7bda33fe8b6254ba712def
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-04-09 00:50:15 +0530

    Upload API

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const model = require('./model');
+const movies = require('./data/web_data.json');
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+	res.render("index");
+})
+
+app.post('/', function(req, res){
+	const userId = req.body.userId;
+	const radio = req.body.radio;
+	console.log(radio);
+	res.redirect('/movies/' + userId +'/' + radio);
+})
+
+app.route('/movies')
+.get(function(req, res) {
+  res.send(movies);
+});
+
+app.get('/movies/:userId/:q', function(req, res){
+  const userId = req.params.userId;
+  const q = req.params.q;
+
+  if (userId < 0 || userId > 610){
+    res.send("No User found!");
+  } else {
+  async function recommend() {
+    try {
+      const recommendations = await model.recommend(userId);
+      if(q === 're'){
+        res.send(recommendations[0]);
+      } else if(q === 'ra'){
+        res.send(recommendations[1]);
+      }
+    } catch (err) {
+      console.error('There was an error ', err);
+    }
+  }
+  const recommendations = recommend(userId);
+}
+})
+
+app.listen(3000, function(){
+    console.log("listening on port 3000...");
+});
---
+++ model.js
@@ -0,0 +1,45 @@
+const tf = require('@tensorflow/tfjs-node');
+const movies = require("./data/web_data.json");
+const movieIds = require("./data/movieId.json");
+
+class L2 {
+    static className = 'L2';
+    constructor(config) {
+       return tf.regularizers.l1l2(config)
+    }
+}
+tf.serialization.registerClass(L2);
+
+async function loadModel() {
+    model = await tf.loadLayersModel("file://model/model.json");
+    // model.summary();
+}
+
+const movieLength = movieIds.length;
+const inputArr = tf.range(0, movieLength);
+
+exports.recommend = async function recommend(userId) {
+    let user = tf.fill([movieLength], Number(userId));
+    await loadModel();
+    predTensor = await model.predict([user, inputArr]);
+    predTensor = predTensor.reshape([-1]);
+
+    const recommendations = tf.topk(predTensor, 120, sorted=true);
+
+    const predRatings = recommendations.values.arraySync();
+    const predIndices = recommendations.indices;
+    const predIds = tf.gather(movieIds, predIndices).arraySync();
+
+    const recommendationsArr = [];
+    predIds.forEach(function(id){
+        const index = movies.findIndex(item => item.movieId === id);
+        recommendationsArr.push(movies[index]);
+    })
+
+    recommendationsArr.forEach(function(i){
+         i.genres = i.genres.split("|")
+         i.year = i.title.slice(-5, -1);
+        });
+
+    return [recommendationsArr, predRatings];
+}
